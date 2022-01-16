dd9f9a9c6e536e5de8a54f8be97c10b6276ddcf6
Tom Miller <git@me.tmiller.dev>
2022-01-16 13:25:42 -0600

    fish: add XDG environment variables

--- roles/local.fish/files/conf.d/shell_exports.fish
+++ roles/local.fish/files/conf.d/shell_exports.fish
@@ -7,3 +7,8 @@ set --global --export KUBECONFIG "$HOME/.kube/config"
 set --global --export DOCKER_SCAN_SUGGEST false
 set --global --export FZF_DEFAULT_COMMAND $FZF_FIND_FILE_COMMAND
 
+# XDG Settings
+set --global --export XDG_CACHE_HOME  $HOME/.cache
+set --global --export XDG_CONFIG_HOME $HOME/.config
+set --global --export XDG_DATA_HOME   $HOME/.local/share
+set --global --export XDG_STATE_HOME  $HOME/.local/state
