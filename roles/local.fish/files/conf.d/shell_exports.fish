c5586bfca50cdb2b7394b12fcfb6522700a7e920
Tom Miller <git@me.tmiller.dev>
2022-01-30 15:58:13 -0600

    gpg: Add XDG support for gpg

--- roles/local.fish/files/conf.d/shell_exports.fish
+++ roles/local.fish/files/conf.d/shell_exports.fish
@@ -12,3 +12,6 @@ set --global --export XDG_CACHE_HOME  $HOME/.cache
 set --global --export XDG_CONFIG_HOME $HOME/.config
 set --global --export XDG_DATA_HOME   $HOME/.local/share
 set --global --export XDG_STATE_HOME  $HOME/.local/state
+
+# Configure GPG to use XDG
+set --global --export GNUPGHOME $HOME/.config/gnupg
