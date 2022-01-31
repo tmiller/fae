623c4006cc14a1fd85342d97a28c4e6b6af0ced5
Tom Miller <git@me.tmiller.dev>
2022-01-30 18:45:09 -0600

    git: Configure gpg signing when adding user
    If there is an email associated with a secret key in gnupg use it to
    sign commits.

--- roles/local.fish/files/functions/git-user-config.fish
+++ roles/local.fish/files/functions/git-user-config.fish
@@ -2,6 +2,19 @@ function git-user-config --description "Configure user for git locally"
   read --local --prompt-str "Enter your name: " name
   read --local --prompt-str "Enter your email: " email
 
+  set --local signing_key (
+    gpg --list-secret-keys $email 2>/dev/null \
+    | rg ssb \
+    | rg '\[S\]' \
+    | awk '{print $2}' \
+    | awk -F/ '{print $2}'
+  )
+
+  if test -n "$signing_key"
+    git config --local user.signingkey $signing_key
+    git config --local commit.gpgsign true
+  end
+
   git config --local user.name $name
   git config --local user.email $email
 end
